f84a6fbb9cfc7ae615f7eeff0beafee394358af3
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-07-09 15:59:00 +0000

    Created Population Program

--- main.cpp
+++ main.cpp
@@ -1,46 +1,59 @@
-//Random Number Guessing Game Program
+//Population Program
 
 #include <iostream>
-#include <random>
+#include <iomanip>
+
 using namespace std;
 
 int main()
 {
-  //Initialize variables and set userNumber and guesses to 0
-  int randomNumber, userNumber, guesses;
-  userNumber = 0;
-  guesses = 1;
-  
-  //Generate a random number between 1 and 100
-  random_device numberEngine;
-  uniform_int_distribution<int> number(1, 100);
-  randomNumber = number(numberEngine);
-
-  //Create a header for the game
-  cout << "Random Number Guessing Game" << endl;
+  //Create a header for the program
+  cout << "Population Size Calculator" << endl;
   cout << endl;
   cout << endl;
   
-  //Ask user to guess a number between 1 and 100
-  cout << "Guess a number between 1 and 100: ";
-  cin >> userNumber;
+  //Set output decimal precision to 0
+  cout << fixed << setprecision(0);
 
-  while (userNumber != randomNumber) {
-    if (userNumber > randomNumber) {
-      cout << "Too high. Try again: ";
-      cin >> userNumber;
-      guesses++;
-    }
+  //Initialize variables
+  int startingSize, averageIncrease, days, population;
 
-    else if (userNumber < randomNumber) {
-      cout << "Too low. Try again: ";
-      cin >> userNumber;
-      guesses++;
-    }
+  //Prompt user to input the starting size of the population.
+  cout << "Enter the starting size of the population: ";
+  cin >> startingSize;
+
+  //Ensure the starting size is at least 2.
+  if (startingSize < 2) {
+    cout << "The starting size must be at least 2." << endl;
+    return 0;
   }
 
-  cout << "Correct! The number was " << randomNumber << ". It took you " << guesses << " guesses." << endl;
+  //Prompt user to input the average daily increase.
+  cout << "Enter the average daily increase (as a percentage). Do not include the % symbol: ";
+  cin >> averageIncrease;
 
+  //Ensure the average daily increase is a positive number.
+  if (averageIncrease < 0) {
+    cout << "The average daily increase must be a positive number." << endl;
+    return 0;
+  }
+
+  //Prompt user to input the number of days they will multiply.
+  cout << "Enter the number of days they will multiply: ";
+  cin >> days;
+
+  //Ensure the number of days is at least 1.
+  if (days < 1) {
+    cout << "The number of days must be at least 1." << endl;
+    return 0;
+  }
+
+  // Calculate the population size for each day and display it
+  for (int i = 1; i <= days; i++) {
+    population = startingSize * (1 + (averageIncrease/100)) *i;
+    cout << "Day " << i << ": " << population << endl;
+  }
+  
 
   return 0;
 }
