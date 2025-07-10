dc3ee8865ad850d37970783e3ede3f49c6adecff
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-07-10 15:54:27 +0000

    .

--- main.cpp
+++ main.cpp
@@ -6,10 +6,7 @@
 using namespace std;
 int main() {
  
-  /*Initialize variables for the student at the front of the line and the student at the end of the line and set them to the extremes of the alphabet. Then initialize a variable for the name of the student being read from the file. Finally, initialize a variable for the number of students in the class and set it to 0.*/
-  string name1 = "Z", name2 = "A";
-  string name;
-  int count = 0;
+  //Initialize variables
   
   //Open the file LineUp.txt that contains the names of the students in the class.
   ifstream inFile;
@@ -21,21 +18,6 @@ int main() {
     return (1);
     }
 
-  //Determine the number of students in the class, the student at the front of the line, and the student at the end of the line.
-  while(inFile >> name){
-    count++;
-    if(name < name1)
-      name1 = name;
-    if(name > name2)
-      name2 = name;
-  }
   
-  //Close the file LineUp.txt.
-  inFile.close();
-
-  //Print the number of students in the class, the student at the front of the line, and the student at the end of the line.
-  cout << "There are " << count << " students in the class." << endl;
-  cout << "The student at the front of the line is " << name1 << "." << endl;
-  cout << "The student at the end of the line is " << name2 << "." << endl;
   
 }
